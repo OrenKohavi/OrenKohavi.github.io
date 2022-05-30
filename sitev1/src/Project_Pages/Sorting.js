c16b70a7e58d096ca27b79f661ffda860f284bd8
Oren Kohavi <oren_kohavi@brown.edu>
2022-05-30 17:26:33 -0400

    polish card animations and interactions

--- sitev1/src/Project_Pages/Sorting.js
+++ sitev1/src/Project_Pages/Sorting.js
@@ -1,5 +1,5 @@
 import './Sorting.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import card_front from '../img/card_front.jpg';
 import { AwesomeButton } from "react-awesome-button";
 import "react-awesome-button/dist/styles.css";
@@ -8,13 +8,28 @@ export default function Sorting() {
     //This may be overuse of useState, but better safe than sorry
     //(Plus, performance impact is minimal with such a simple page)
     let cards_created = 0;
+    const max_card_value = 100;
     const [card_values, setCardValues] = useState([]);
     const [num_cards, setNumCards] = useState(7);
     const [operations, setOperations] = useState(0);
     const [flipArray, setFlipArray] = useState(Array(num_cards).fill(false));
     const [flipAllowed, setFlipAllowed] = useState(true);
     const [alertedRotation, setAlertedRotation] = useState(false);
-    const [doneDisabled, setDoneDisabled] = useState(true);
+    const [finishopEnabled, setFinishopEnabled] = useState(false);
+
+    useEffect(() => {
+        //set nextopEnabled to true when flipAllowed is false
+        if (!flipAllowed) {
+            setFinishopEnabled(true);
+        } else {
+            setFinishopEnabled(false);
+        }
+    }, [flipAllowed]);
+
+    useEffect(() => {
+        //re-generate card values when num_cards changes
+        generate_card_values();
+    }, [num_cards]);
 
     let on_cardflip = () => {
         //This function is called before the card is set to flipped in flipArray
@@ -24,7 +39,6 @@ export default function Sorting() {
             //Don't allow any more flips
             setFlipAllowed(false);
         }
-        //TODO Logic for checking if 2 cards are flipped and stuff
     }
 
     let flip_card_at_idx = (idx) => {
@@ -33,20 +47,50 @@ export default function Sorting() {
         setFlipArray(new_flip_array);
     }
 
+    let num_cards_changed = () => {
+        //TODO: Provide a warning that changing the number of cards resets the game
+        //only proceed if the user is sure
+
+        //for now, just reset the cards
+        reset_button_click();
+        return true; //For now, always return true
+    }
+
+    let reset_button_click = () => {
+        //Reset the cards and the operations counter
+        //Also re-generate the card values
+        unflip_cards();
+        generate_card_values();
+        setOperations(0);
+    }
+
+    let finishop_button_click = () => {
+        //This function is called when the done button is clicked
+        //TODO: Check if the cards are in the correct order
+        unflip_cards();
+        setOperations(operations + 1);
+    }
+
     let unflip_cards = () => {
         setFlipArray(Array(num_cards).fill(false))
         setFlipAllowed(true);
     }
 
     let generate_card_values = () => {
-        //Hacky fix: Sometimes num_cards doesn't update in time, leaving the last card without a value
-        //No harm in making the list too long though (unused values are simply never accessed)
-        //So generate an array slightly too long, just in case (starting i at -2)
         let new_card_array = [];
-        for (let i = -2; i < num_cards; i++) {
-            new_card_array.push(Math.floor(Math.random() * 100));
+        for (let i = 0; i < num_cards; i++) {
+            //Range from 1 to max_card_value
+            new_card_array.push(Math.floor(Math.random() * max_card_value) + 1);
         }
         setCardValues(new_card_array)
+    }
+
+    let render_all_cards = () => {
+        let cards = [];
+        for (let i = 0; i < num_cards; i++) {
+            cards.push(renderCard(i));
+        }
+        return cards;
     }
 
 
@@ -64,16 +108,6 @@ export default function Sorting() {
         )
     }
 
-    let render_all_cards = () => {
-        if (card_values.length === 0){
-            generate_card_values();
-        }
-        let cards = [];
-        for (let i = 0; i < num_cards; i++) {
-            cards.push(renderCard(i));
-        }
-        return cards;
-    }
 
     if (window.innerWidth < window.innerHeight && !alertedRotation) {
         alert("This page is designed for a desktop or laptop screen. Please rotate your device to landscape mode.");
@@ -92,28 +126,30 @@ export default function Sorting() {
                     <h2>
                         <label className="card-number-select-label" for="card-number-select">How many cards: </label>
                         <input className="card-number-select" type="number" min="4" max="15" defaultValue="7" onChange={(e) => {
-                            setNumCards(e.target.value);
-                            unflip_cards();
-                            generate_card_values();
-                            render_all_cards();
+                            let user_confirmation = num_cards_changed();
+                            if (user_confirmation) {
+                                setNumCards(e.target.value);
+                            }
                         }}/>
                     </h2>
                     <h2>Number Of Operations: {operations}</h2>
                 </div>
                 <div className="sorting-instructions">
                     <p>Soon, this will be an interactive display showing the next step to take</p>
-                    <p>Select 2 Cards ➡️ Drag Cards Around ➡️ Click 'Done' ➡️ Repeat</p>
+                    <p>Select 2 Cards ➡️ Drag Cards Around ➡️ Click 'Finish Operation' ➡️ Repeat</p>
                 </div>
                 <div className="sorting-buttons">
                     <div className="reset-button-div">
-                        <AwesomeButton type="primary" ripple={true} disabled={false} onPress={unflip_cards}>Restart Game</AwesomeButton>
+                        <AwesomeButton type="primary" ripple onPress={reset_button_click}>Restart Game</AwesomeButton>
                     </div>
                     <div className="next-button-div">
-                        <AwesomeButton type="primary" disabled={doneDisabled} onPress={unflip_cards}>Next Operation</AwesomeButton>
+                        <AwesomeButton type="primary" disabled={!finishopEnabled} onPress={finishop_button_click}>Finish Operation</AwesomeButton>
                     </div>
                 </div>
-                <div className="sorting-game-cards">
-                    {render_all_cards()}
+                <div className="cards-and-blocker">
+                    <div className="sorting-game-cards">
+                        {render_all_cards()}
+                    </div>
                 </div>
             </div>
         </div>
