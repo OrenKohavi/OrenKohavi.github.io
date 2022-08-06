102e3a72a45eba069cb2371f1e0bed3bf4ce6c87
Oren Kohavi <orenkohavi@live.com>
2022-08-06 19:21:17 +0300

    remove mobile version of family tree

--- sitev1/src/pages/FamilyTree.js
+++ sitev1/src/pages/FamilyTree.js
@@ -2,18 +2,6 @@ import FamilyTreeImage from '../assets/FamilyTree.png';
 import { isMobile } from 'react-device-detect';
 
 export default function FamilyTree() {
-    if (isMobile) {
-        return (
-            <div className="nah">
-                <br></br>
-                <br></br>
-                <br></br>
-                <h1 className="centered">
-                    I'm way too lazy to figure out how to make this a mobile site just view it on your laptop.
-                </h1>
-            </div>
-        )
-    }
     return (
         <div className="family-tree">
             <br></br>
